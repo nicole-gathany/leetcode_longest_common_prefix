75115e012250fb947135405b6746c72682e6c920
ncgmph2014 <nicole.gathany@gmail.com>
2020-02-25 13:02:31 -0500

    another test that mightget me closer

--- index.js
+++ index.js
@@ -3,15 +3,15 @@
  * @return {string}
  */
 var longestCommonPrefix = function(strs) {
-  //   for (let i = 0; i < strs.length; i++) {
-  //     //syntax for substring string.substr(start, length)
-  //     for (let j = strs[i].length; j >= 0; j--) {
-  //         // when i tested this i wasn't sure
-  //       console.log(strs[i].substr(0, j));
-  //     }
-  //   }
+  for (let i = 0; i < strs.length; i++) {
+    //syntax for substring string.substr(start, length)
+    //for (let j = strs[i].length; j >= 0; j--) {
+    // when i tested this i wasn't sure
+    console.log(strs[i].substr(0, 3));
+    //}
+  }
 
-  return strs.every(x => x.substr(0, 3));
+  //   return strs.every(x => x.substr(0, 3));
 };
 
 console.log(longestCommonPrefix(["flower", "flow", "flight"]));
