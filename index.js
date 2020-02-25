b1ce3c6d87fc7142aa7aebdc005acb0d5425c830
ncgmph2014 <nicole.gathany@gmail.com>
2020-02-25 13:24:58 -0500

    i don't know how any of this works!!

--- index.js
+++ index.js
@@ -7,11 +7,13 @@ var longestCommonPrefix = function(strs) {
     //syntax for substring string.substr(start, length)
     //for (let j = strs[i].length; j >= 0; j--) {
     // when i tested this i wasn't sure
-    console.log(strs[i].substr(0, 3));
+    console.log(strs[i].includes(x => x.substring(0, 2)));
     //}
   }
-
-  //   return strs.every(x => x.substr(0, 3));
+  //which string is the longest
+  for (let i = 0; i < strs.length; i++) {}
+  //this doesn't work the way i wanted it to
+  //   return strs.every(x => x.substring(0, 3));
 };
 
 console.log(longestCommonPrefix(["flower", "flow", "flight"]));
