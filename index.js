62a15c50ca99b023871580f9dd2a75f372c221f5
ncgmph2014 <nicole.gathany@gmail.com>
2020-02-25 15:29:35 -0500

    not sure actually

--- index.js
+++ index.js
@@ -9,31 +9,20 @@ var longestCommonPrefix = function(strs) {
   }
 
   arrLengths.sort((a, b) => a - b);
+  //arrLengths[0] is the shortest length
   console.log(arrLengths[0]);
 
   //maybe we can make an array with all the substrings
   let substringArr = [];
-  for (let i = 0; i < strs.length; i++) {
-    //syntax for substring string.substr(start, length)
-    //going to say that arrLengths[0] is the limit because we don't need anything bigger than that
-    for (let j = arrLengths[0]; j >= 0; j--) {
-      substringArr.push(strs[i].substr(0, j));
-    }
+  //syntax for substring string.substr(start, length)
+  //going to say that arrLengths[0] is the limit because we don't need any substring bigger than that
+  // decided to only use the first string because we really only need to examine the substrings from one string of the original array
+  //want to limit nested arrays to make the function more efficient
+  for (let j = arrLengths[0]; j >= 0; j--) {
+    substringArr.push(strs[0].substr(0, j));
   }
 
   console.log(substringArr);
-  //actually i just want to know which one is the shortest
-
-  //arrLengths[0] is the shortest length
-
-  //   for(let i=arrLengths; i>=0; i--){
-
-  //   }
-  //this doesn't work the way i wanted it to
-  //   return strs.every(x => x.substring(0, 3));
-
-  //we want to know if every string within strs includes a particular substring
-  // if it does, return that substring
 };
 
 console.log(longestCommonPrefix(["flower", "flow", "flight"]));
