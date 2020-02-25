4595d5bdf25c4fae907c9b1c3c8b2133ab23d456
ncgmph2014 <nicole.gathany@gmail.com>
2020-02-25 13:00:16 -0500

    added an every method; don't think it worked

--- index.js
+++ index.js
@@ -3,13 +3,16 @@
  * @return {string}
  */
 var longestCommonPrefix = function(strs) {
-  for (let i = 0; i < strs.length; i++) {
-    //syntax for substring string.substr(start, length)
-    for (let j = strs[i].length; j >= 0; j--) {
-      console.log(strs[i].substr(0, j));
-    }
-  }
+  //   for (let i = 0; i < strs.length; i++) {
+  //     //syntax for substring string.substr(start, length)
+  //     for (let j = strs[i].length; j >= 0; j--) {
+  //         // when i tested this i wasn't sure
+  //       console.log(strs[i].substr(0, j));
+  //     }
+  //   }
+
+  return strs.every(x => x.substr(0, 3));
 };
 
-longestCommonPrefix(["flower", "flow", "flight"]);
-longestCommonPrefix(["dog", "racecar", "car"]);
+console.log(longestCommonPrefix(["flower", "flow", "flight"]));
+console.log(longestCommonPrefix(["dog", "racecar", "car"]));
