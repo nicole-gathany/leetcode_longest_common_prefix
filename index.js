b597390e0ac3b86c94c01ed0996eb7ffd8316f3e
ncgmph2014 <nicole.gathany@gmail.com>
2020-02-25 15:18:27 -0500

    created an array with all the substrings

--- index.js
+++ index.js
@@ -3,15 +3,18 @@
  * @return {string}
  */
 var longestCommonPrefix = function(strs) {
-  //   for (let i = 0; i < strs.length; i++) {
-  //     //syntax for substring string.substr(start, length)
-  //     for (let j = strs[i].length; j >= 0; j--) {
-  //       // when i tested this i wasn't sure
-  //       //i don't know how eny of this works!!
-  //       console.log(strs[i].substr(0, j));
-  //     }
-  //     console.log(strs[i]);
-  //   }
+  //maybe we can make an array with all the substrings
+  let substringArr = [];
+  for (let i = 0; i < strs.length; i++) {
+    //syntax for substring string.substr(start, length)
+    for (let j = strs[i].length; j >= 0; j--) {
+      // when i tested this i wasn't sure
+      //i don't know how eny of this works!!
+      substringArr.push(strs[i].substr(0, j));
+    }
+  }
+
+  console.log(substringArr);
   //actually i just want to know which one is the shortest
   let arrLengths = [];
   for (let i = 0; i < strs.length; i++) {
@@ -21,14 +24,15 @@ var longestCommonPrefix = function(strs) {
   arrLengths.sort((a, b) => a - b);
   console.log(arrLengths[0]);
   //arrLengths[0] is the shortest length
-  let test = strs.every(x => x.includes(x.substring(0, arrLengths)));
-  //test is true for all the strings and that should not be the case
-  console.log(test);
+
   //   for(let i=arrLengths; i>=0; i--){
 
   //   }
   //this doesn't work the way i wanted it to
   //   return strs.every(x => x.substring(0, 3));
+
+  //we want to know if every string within strs includes a particular substring
+  // if it does, return that substring
 };
 
 console.log(longestCommonPrefix(["flower", "flow", "flight"]));
